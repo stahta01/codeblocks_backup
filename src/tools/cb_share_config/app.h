0e487a45aa0f7f6376b91043c083a59087209e78
Tim S <stahta01@users.sourceforge.net>
2015-03-23 22:58:51 -0400

    Removed including of wx/wxprec.h and wx/wx.h from header and added include of wx/app.h.

--- src/tools/cb_share_config/app.h
+++ src/tools/cb_share_config/app.h
@@ -6,20 +6,12 @@
 #ifndef APP_H
 #define APP_H
 
-#include <wx/wxprec.h>
-
-#ifdef __BORLANDC__
-	#pragma hdrstop
-#endif
-
-#ifndef WX_PRECOMP
-	#include <wx/wx.h>
-#endif
+#include <wx/app.h>
 
 class MyApp : public wxApp
 {
-	public:
-		virtual bool OnInit();
+    public:
+        virtual bool OnInit();
 };
 
 #endif // APP_H
