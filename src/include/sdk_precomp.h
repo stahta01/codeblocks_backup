857f5220c177933bcf499c6ba6b82433d78d8c30
Tim S <stahta01@users.sourceforge.net>
2015-10-11 13:25:24 -0400

    DO NOT USE UPSTREAM: Testing single PCH of sdk_precomp.h code changes.

--- src/include/sdk_precomp.h
+++ src/include/sdk_precomp.h
@@ -10,6 +10,19 @@
 #ifndef SDK_PRECOMP_H
 #define SDK_PRECOMP_H
 
-#include "sdk_common.h"
+
+#if defined(__WXMSW__) || defined(__WINDOWS__) || defined(_WIN32)
+    #ifdef CB_PRECOMP
+        #define EXPORT_LIB
+        #define EXPORT_EVENTS
+        #define WXUSINGDLL
+        #define WXMAKINGDLL_SCI
+    #endif
+
+    #include "sdk.h" // Included instead of sdk_common.h to define the header guard for sdk.h
+#else
+    // for non-windows platforms, one PCH is enough
+    #include "sdk_common.h"
+#endif
 
 #endif // SDK_PRECOMP_H
